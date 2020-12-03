9b8c68d9aade475b9d990753d3cfa5ec3ec6bba4
Robert Caraballo <62614866+Trevolt11@users.noreply.github.com>
2020-12-03 08:31:14 -0500

    Add files via upload

---
+++ clean_arch/Api/Controllers/VehiculoApisController.cs
@@ -0,0 +1,32 @@
+﻿using infraestructura.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+using Core.interfaces;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoApiController : ControllerBase
+
+    {
+
+        public readonly InterfaceVehiculoRepositorio _vehiculoRepositorio;
+
+        //Construtor
+        public VehiculoApiController(InterfaceVehiculoRepositorio vehiculoRepositorio)
+        {
+            _vehiculoRepositorio = vehiculoRepositorio;
+
+        }
+
+
+        [HttpGet]
+        public async Task <IActionResult> GetVehiculoApi()
+        {
+            var VehiculoEn = await _vehiculoRepositorio.GetVehiculos();
+            return Ok(VehiculoEn);
+        }
+
+    }
+}
---
+++ clean_arch/Api/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.interfaces;
+using Core.entidades;
+using infraestructura.Repositorios;
+using infraestructura.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //Resolucion de dependencias ----------------------------------------------
+
+              services.AddDbContext<Clean_ArchContext>(Options =>
+              Options.UseSqlServer(Configuration.GetConnectionString("Clean_archVehi")));
+
+            services.AddTransient<InterfaceVehiculoRepositorio, VehiculoRepositorio>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
